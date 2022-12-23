e3312900242f84d78dea835a7474b0d59e579a0b
Vaibhav Brid <vaibhav.brid@gmail.com>
2022-12-22 16:59:00 -0800

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using CoreWEbApp.Models;
+using CoreWEbApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreWEbApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductsService productsService = new();
+            Products = productsService.GetProducts();
+        }
+    }
+}
---
+++ Services/ProductsService.cs
@@ -0,0 +1,49 @@
+﻿using CoreWEbApp.Models;
+using System.Data.SqlClient;
+
+namespace CoreWEbApp.Services
+{
+    public class ProductsService
+    {
+        private static string db_server = "apps-services-net7-vai.database.windows.net";
+        private static string db_user = "azadmin";
+        private static string db_password = "azureAdmin1@#";
+        private static string db_database = "Northwind";
+
+        private SqlConnection GetConnection()
+        {
+            var sqlStringBuilder = new SqlConnectionStringBuilder();
+            sqlStringBuilder.DataSource = db_server;
+            sqlStringBuilder.UserID = db_user;
+            sqlStringBuilder.Password = db_password;
+            sqlStringBuilder.InitialCatalog = db_database;
+            return new SqlConnection(sqlStringBuilder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection connection = GetConnection();
+            List<Product> products = new List<Product>();
+
+            string sqlStatement = "SELECT ProductId, ProductName from Products";
+
+            connection.Open();
+
+            SqlCommand sqlCommand = new(sqlStatement, connection);
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new()
+                    {
+                        ProductId = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                    };
+
+                    products.Add(product);
+                }
+            }
+            return products;
+        }
+    }
+}
